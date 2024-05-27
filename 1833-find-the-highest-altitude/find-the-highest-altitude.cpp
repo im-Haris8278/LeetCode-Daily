72d1c46462e6bd4f0cbdb60ded48d7cede8450c1
Haris Baig <bsf2100818@ue.edu.pk>
2024-05-27 21:00:19 +0500

    Time: 0 ms (100.00%) | Memory: 9.7 MB (88.32%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int largestAltitude(vector<int>& gain) {
+    int largestAltitude(vector<int>& gain) 
+    {
         int currentAltitude = 0;
         int maxAltitude = 0;
 
@@ -9,7 +10,7 @@ public:
             currentAltitude += g;
             maxAltitude = max(maxAltitude, currentAltitude);
         }
-        
+
         return maxAltitude;
     }
 };
