93b1e898df823396cf2f11fd9e078e1f778066bc
Haris Baig <bsf2100818@ue.edu.pk>
2024-05-31 20:40:58 +0500

    Time: 8 ms (64.59%) | Memory: 8.4 MB (83.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,31 +2,14 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
-        if (s.length() != t.length()) 
+        if (s.length() != t.length())
         {
             return false;
         }
-        
-        unordered_map<char, int> frequencyS, frequencyT;
-        for (char c : s) 
-        {
-            frequencyS[c]++;
-        }
 
-        for (char c : t) 
-        {
-            frequencyT[c]++;
-        }
-        
-        for (const auto& pair : frequencyS) 
-        {
-            char c = pair.first;
-            if (frequencyT.find(c) == frequencyT.end() || frequencyT[c] != pair.second)
-            {
-                return false;
-            }
-        }
-        
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
     }
 };
