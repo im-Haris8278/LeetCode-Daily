ad06a35dc2ddd575267d04d8c6d96169df06f59a
Haris Baig <bsf2100818@ue.edu.pk>
2024-06-16 14:02:35 +0500

    Time: 55 ms (50.00%) | Memory: 49.1 MB (73.08%) - LeetSync

---
+++ 330-patching-array/patching-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number}
+ */
+var minPatches = function(nums, n) 
+{
+    let miss = 1;
+    let patches = 0;
+    let i = 0;
+
+    while (miss <= n)
+    {
+        if (i < nums.length && nums[i] <= miss)
+        {
+            miss += nums[i];
+            i++;
+        }
+        else
+        {
+            miss += miss;
+            patches++;
+        }
+    }
+
+    return patches;
+};
