af5bf8c5efcfd4d69b348fe74e2da86e52bf16dc
Haris Baig <bsf2100818@ue.edu.pk>
2024-06-27 23:46:18 +0500

    Time: 218 ms (95.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    (SELECT 
+        DISTINCT salary
+    FROM 
+        employee
+    ORDER BY   
+        salary DESC
+    LIMIT 1
+    offset 1)
+    AS SecondHighestSalary;
